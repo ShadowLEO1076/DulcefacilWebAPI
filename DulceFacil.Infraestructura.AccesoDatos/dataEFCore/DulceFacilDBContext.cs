28f2091372f36e3eaa687d8db179bc01005e32ff
ShadowLEO1076 <guillermoleonardocalvache@gmail.com>
2025-06-22 14:47:19 -0500

    <actualice>

--- DulceFacil.Infraestructura.AccesoDatos/dataEFCore/DulceFacilDBContext.cs
+++ DulceFacil.Infraestructura.AccesoDatos/dataEFCore/DulceFacilDBContext.cs
@@ -109,7 +109,7 @@ public partial class DulceFacilDBContext : DbContext
 
             entity.HasOne(d => d.idRutaNavigation).WithMany(p => p.Entrega)
                 .HasForeignKey(d => d.idRuta)
-                .HasConstraintName("FK__Entrega__idRuta__75A278F5");
+                .HasConstraintName("FK__Entrega__idRuta__75A278F5");l
         });
 
         modelBuilder.Entity<Pedido>(entity =>
